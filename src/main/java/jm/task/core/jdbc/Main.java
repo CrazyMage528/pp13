fc86d52158fa3aa4205bd7d3001e7efab0c0bbd4
CrazyMage528 <ursaev.r@yandex.ru>
2024-09-01 22:57:22 +0500

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,32 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        // Создание таблицы User(ов)
+        userService.createUsersTable();
+
+        // Добавление 4 User(ов) в таблицу с данными на свой выбор
+        userService.saveUser("John", "Doe", (byte) 30);
+        System.out.println("User с именем John добавлен в базу данных");
+        userService.saveUser("Jane", "Doe", (byte) 25);
+        System.out.println("User с именем Jane добавлен в базу данных");
+        userService.saveUser("Jim", "Beam", (byte) 40);
+        System.out.println("User с именем Jim добавлен в базу данных");
+        userService.saveUser("Jack", "Daniels", (byte) 35);
+        System.out.println("User с именем Jack добавлен в базу данных");
+
+        // Получение всех User из базы и вывод в консоль
+        userService.getAllUsers().forEach(System.out::println);
+
+        // Очистка таблицы User(ов)
+        userService.cleanUsersTable();
+
+        // Удаление таблицы
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // URL, логин и пароль для подключения к базе данных
+    private static final String URL = "jdbc:mysql://localhost:3306/new_schema";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "123123";
+
+    // Метод для получения соединения с базой данных
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Не удалось установить соединение с базой данных");
+        }
+        return connection;
+    }
 }
